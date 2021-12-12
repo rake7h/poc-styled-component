d179da35006045b4f28675fc1dd8ba77d4bbea04
Rakesh <rakesh.singh2@go-mmt.com>
2021-12-12 18:04:21 +0530

    raw css in styled-component

--- webpack.client.prod.js
+++ webpack.client.prod.js
@@ -12,6 +12,10 @@ module.exports = {
   module: {
     rules: [
       {
+        resourceQuery: /raw/,
+        type: 'asset/source'
+      },
+      {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
